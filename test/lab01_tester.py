ce3f6a23bd02fb0d6922f435c9b79cfde22a4001
thanosk <cse32467@cs.uoi.gr>
2016-02-22 14:41:05 +0200

    lab1 tester commit1

--- test/lab01_tester.py
+++ test/lab01_tester.py
@@ -11,9 +11,9 @@ else:
   # MODIFY FOR YOUR PERSONAL COMPUTER SETUP!
   #  USING THE FULL PATH TO MarsMYY402_4_5.jar IN YOUR COMPUTER
   ##########################################
-  marsJar = "/home/efthym/t/ca_course/MarsMYY402_4_5.jar"
+  marsJar = "C:\\Users\\Toshiba\\Desktop\\Mars4_5.jar"
 
-matric = 1000
+matric = 2467
 tests = [ \
  ("Matric simple test",\
   ["s/^matric:.*$/matric: .word %d/" %(matric) ],\
@@ -24,4 +24,4 @@ tests = [ \
 
   
 # Change verbose below to True, if you get errors to get more detailed information
-print (runTests("../lab01.asm", tests, marsJar, verbose=False))
+print (runTests("../lab01.asm", tests, marsJar, verbose=True))
